eb8e81e9547431c33431a890470d8e123cbdd11c
takahashi <coudai3141xyzww@gmail.com>
2025-03-29 12:55:52 +0900

    second

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContactController;
+
+
+Route::get('/', [ContactController::class, 'index']);
+Route::POST('/contacts/confirm', [ContactController::class, 'confirm']);
+Route::post('/contacts', [ContactController::class, 'store']);
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +19,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
